c710b237345d1d3c2c74a8193fc17c30960eb2f2
Tom King <ka6sox@gmail.com>
2015-07-20 11:09:01 -0700

    add all files

---
+++ hardware/sdr_pre.cmd
@@ -0,0 +1,50 @@
+# Pin name action command file
+
+# Start of element U1
+ChangePinName(U1, 3, OUT)
+ChangePinName(U1, 1, IN)
+ChangePinName(U1, 2, GND)
+ChangePinName(U1, 4, GND)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element SH1
+ChangePinName(SH1, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 2, 2)
+ChangePinName(J1, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 1, 1)
+ChangePinName(C4, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 1, 1)
+ChangePinName(C3, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 1, 1)
+ChangePinName(C2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 1, 1)
+ChangePinName(C5, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 1, 1)
+ChangePinName(C1, 2, 2)
